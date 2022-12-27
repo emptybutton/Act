43da286454aa33690506f57eceae3836eaa33809
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-27 23:07:04 +0700

    Class that implements checking whether an object conforms to types

---
+++ pyhandling/checkers.py
@@ -0,0 +1,32 @@
+from typing import Iterable
+
+
+class TypeChecker:
+    """
+    Class that implements checking whether an object conforms to certain types
+
+    Has the is_correctness_under_supertype flag attribute that specifies whether
+    the object type should match the all support types.
+    """
+
+    def __init__(
+        self,
+        correct_type_resource: Iterable[type] | type,
+        *,
+        is_correctness_under_supertype: bool = False
+    ):
+        self.is_correctness_under_supertype = is_correctness_under_supertype
+        self.correct_types = (
+            correct_type_resource
+            if isinstance(correct_type_resource, Iterable)
+            else (correct_type_resource, )
+        )
+
+    def __call__(self, resource: any) -> bool:
+        return (
+            len(self.correct_types) == 0
+            and (all if self.is_correctness_under_supertype else any)(
+                isinstance(resource, correct_type)
+                for correct_type in self.correct_types
+            )
+        )
