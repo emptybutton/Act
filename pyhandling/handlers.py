cb668c62913b90366d8ecfc64d3fe0d96de0020e
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-27 23:03:37 +0700

    Handler proxy class to use them multiple handling as a single handler interface

--- pyhandling/handlers.py
+++ pyhandling/handlers.py
@@ -11,3 +11,32 @@ class HandlerKeeper:
             if isinstance(handler_resource, Iterable)
             else (handler_resource, )
         ) + handlers
+
+
+class MultipleHandler(HandlerKeeper):
+    """
+    Handler proxy class for representing multiple handlers as a single
+    interface.
+
+    Has the is_return_delegated flag attribute to enable or disable returning
+    the result of one from the handlers.
+
+    When one handler returns anything other than None, it returns that value,
+    breaking the loop for other handlers.
+    """
+
+    def __init__(
+        self,
+        handler_resource: Handler | Iterable[Handler],
+        *handlers: Handler,
+        is_return_delegated: bool = True
+    ):
+        super().__init__(handler_resource, *handlers)
+        self.is_return_delegated = is_return_delegated
+
+    def __call__(self, resource: any) -> any:
+        for handler in self.handlers:
+            result = handler(resource)
+
+            if self.is_return_delegated and result is not None:
+                return result
