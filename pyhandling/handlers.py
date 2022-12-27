8f13b7296406f2cf1642f8c6c70f005ce0b7864c
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-27 23:02:11 +0700

    Handler keeper class to conveniently accept them

--- pyhandling/handlers.py
+++ pyhandling/handlers.py
@@ -2,3 +2,12 @@ from typing import NewType, Callable, Iterable, Self, Optional
 
 
 Handler = NewType('Handler', Callable[[any], any])
+
+
+class HandlerKeeper:
+    def __init__(self, handler_resource: Handler | Iterable[Handler], *handlers: Handler):
+        self.handlers = (
+            tuple(handler_resource)
+            if isinstance(handler_resource, Iterable)
+            else (handler_resource, )
+        ) + handlers
