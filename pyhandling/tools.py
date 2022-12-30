c099c2de68ed2dc9542bcffe74bb1e993e57008c
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2022-12-30 23:24:00 +0700

    Delegating descriptor

--- pyhandling/tools.py
+++ pyhandling/tools.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from math import inf
 from typing import Iterable, Self, Callable
 
 
@@ -26,4 +27,30 @@ class Arguments:
     def create_via_call(cls, *args, **kwargs) -> Self:
         return cls(args, kwargs)
 
+
+class DelegatingProperty:
+    def __init__(
+        self,
+        delegated_attribute_name: str,
+        *,
+        settable: bool = False,
+        value_converter: Callable[[any], any] = lambda resource: resource
+    ):
+        self.delegated_attribute_name = delegated_attribute_name
+        self.settable = settable
+        self.value_converter = value_converter
+
+    def __get__(self, instance: object, owner: type) -> any:
+        return getattr(instance, self.delegated_attribute_name)
+
+    def __set__(self, instance: object, value: any) -> None:
+        if not self.settable:
+            raise AttributeError(
+                "delegating property of '{attribute_name}' for '{class_name}' object is not settable".format(
+                    attribute_name=self.delegated_attribute_name,
+                    class_name=type(instance).__name__
+                )
+            )
+
+        setattr(instance, self.delegated_attribute_name, self.value_converter(value))
 
