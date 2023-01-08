5f2af5245660c3135482293c36f520a5190ff8b7
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-08 22:41:06 +0700

    Tests for synonyms.py

---
+++ tests/test_synonyms.py
@@ -0,0 +1,91 @@
+from functools import partial
+from typing import Optional, Type, Callable, Iterable
+
+from pyhandling.synonyms import return_, raise_, call, call_method, getattr_of, setattr_of, getitem_of, setitem_of, execute_operation, handle_context_by
+
+from pytest import mark, raises
+
+
+class Box:
+    def __init__(self, enter_result: any = None, **attributes):
+        self.__dict__ = attributes
+        self.enter_result = enter_result
+
+    def __repr__(self) -> str:
+        return '<Box instance>'
+
+    def __enter__(self) -> any:
+        return self.enter_result
+
+    def __exit__(self, error_type: Optional[Type[Exception]], error: Optional[Exception], traceback: any):
+        pass
+
+
+def add_then_divide_by_zero_f_attribute(adding_number: int, object_: object) -> None:
+    object_.f += adding_number
+    object_.f /= 0
+
+
+@mark.parametrize('resource', (0, 1, 2))
+def test_return_(resource: any):
+    assert return_(resource) == resource
+
+
+@mark.parametrize('error', (Exception, ))
+def test_raise_(error: Exception):
+    with raises(error):
+        raise_(error)
+
+
+@mark.parametrize('func, result', ((partial(return_, 1), 1), (lambda: 1 + 2, 3)))
+def test_call(func: Callable[[], any], result: any):
+    assert call(func) == result
+
+
+@mark.parametrize('result, object_, method_name', (('<Box instance>', Box(), '__repr__'), ))
+def test_call_method(result: any, object_: object, method_name: str):
+    assert call_method(object_, method_name) == result
+
+
+@mark.parametrize('object_, attribute_name, result', ((Box(a=1), 'a', 1), (Box(b=2), 'b', 2)))
+def test_getattr_of(object_: object, attribute_name: str, result: any):
+    assert getattr_of(object_, attribute_name) == result
+
+
+@mark.parametrize('object_, attribute_name, attribute_value', ((Box(), 'a', 1), (Box(), 'b', 2)))
+def test_setattr_of(object_: object, attribute_name: str, attribute_value: any):
+    setattr_of(object_, attribute_name, attribute_value)
+
+    assert getattr(object_, attribute_name) == attribute_value
+
+
+@mark.parametrize('object_, key, result', ((dict(a=1), 'a', 1), (dict(b=2), 'b', 2)))
+def test_getitem_of(object_: object, key: any, result: any) -> any:
+    assert getitem_of(object_, key) == result
+
+
+@mark.parametrize('object_, key, value', ((dict(), 'a', 1), (dict(), 'b', 2)))
+def setitem_of(object_: object, key: any, value: any) -> None:
+    setitem_of(object_, key, value)
+
+    assert object_[key] == value
+
+
+@mark.parametrize('first, operator, second, result', ((1, '+', 2, 3), (2, '-', 1, 1), (4, '**', 4, 256)))
+def test_execute_operation(first: any, operator: str, second: any, result: any):
+    assert execute_operation(first, operator, second) == result
+
+
+@mark.parametrize(
+    'context_factory, context_handler, result', (
+        (Box, lambda resource: resource, None),
+        (partial(Box, 1), lambda resource: resource, 1),
+        (partial(Box, 2), lambda number: number * 2, 4)
+    )
+)
+def test_handle_context_by(
+    context_factory: Callable[[], any],
+    context_handler: Callable[[any], any],
+    result: any
+):
+    assert handle_context_by(context_factory, context_handler) == result
