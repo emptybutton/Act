67510d2417a88c1ff2666a58566859bd0dfae40f
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-10 21:35:07 +0700

    Test for to_clone

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,3 +1,6 @@
+from pyhandling.tools import to_clone, ArgumentPack, ArgumentKey, DelegatingProperty
+
+
 class MockObject:
     def __init__(self, **attributes):
         self.__dict__ = attributes
@@ -6,3 +9,15 @@ class MockObject:
         return "<MockObject with {attributes}>".format(
             attributes=str(self.__dict__)[1:-1].replace(': ', '=').replace('\'', '')
         )
+
+
+def test_to_clone():
+    object_ = MockObject(mock_attribute=42)
+
+    cloned_object = to_clone(setattr)(object_, 'mock_attribute', 4)
+
+    assert object_ is not cloned_object
+    assert object_.mock_attribute == 42
+    assert cloned_object.mock_attribute == 4
+
+
