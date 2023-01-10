0a4f100800150a4d046d4cfd567052539f4dbc4c
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-10 21:32:04 +0700

    Test for TypeChecker

--- tests/test_checkers.py
+++ tests/test_checkers.py
@@ -18,3 +18,23 @@ def test_negationer(checker: Callable[[any], bool], resource: any):
     assert not Negationer(checker)(resource) is checker(resource)
 
 
+@mark.parametrize(
+    'type_, resource',
+    [
+        (int, int()),
+        (str, int()),
+        (int | str, int()),
+        (int | str, str()),
+        (int | str, float()),
+        (int | str | float | set | frozenset, range(0)),
+        (Union[set, frozenset], set),
+        (Union[set, frozenset], frozenset),
+        (Union[set, frozenset, int, float, str, range, tuple], 1j),
+        (Union[Iterable, int, str, Mapping], tuple()),
+        (Union[Iterable, int, str, Mapping], 1j)
+    ]
+)
+def test_type_checker(type_: type, resource: any):
+    assert TypeChecker(type_)(resource) == isinstance(resource, type_)
+
+
