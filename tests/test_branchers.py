da4cb44096f1825bef3ca8b3a3d03bc64e9a6aa0
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-10 21:30:07 +0700

    Test pseudo then statement

--- tests/test_branchers.py
+++ tests/test_branchers.py
@@ -439,3 +439,22 @@ def test_eventually(binded_numbers: Iterable[int | float], not_counted_numbers: 
     )(*not_counted_numbers) == sum(binded_numbers)
 
 
+@mark.parametrize(
+    'opening_handler, node_handler, input_args',
+    [
+        (lambda x, y: x + y, lambda x: x ** x, (5, 3)),
+        (lambda x: x**2 + 12, lambda x: x ** 4, (42, )),
+        (lambda: 12, lambda x: x + 30, tuple())
+    ]
+)
+def test_then_operator(
+    opening_handler: Callable,
+    node_handler: Callable,
+    input_args: Iterable
+):
+    assert (
+        (opening_handler |then>> node_handler)(*input_args)
+        == ActionChain(opening_handler, node_handler)(*input_args)
+    )
+
+
