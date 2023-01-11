316fc9ebcfa953468960a7deae3cd6e1a55db460
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-11 21:17:53 +0700

    Test for times

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -74,3 +74,19 @@ def test_as_collection(input_resource: any, result: tuple):
     assert as_collection(input_resource) == result
 
 
+@mark.parametrize(
+    "steps_to_false, number_of_runs",
+    [(3, 12), (0, 10), (1, 4), (10, 33)]
+)
+def test_times(steps_to_false: int, number_of_runs: int):
+    runner = times(steps_to_false)
+
+    inital_steps_to_false = steps_to_false
+
+    for _ in range(inital_steps_to_false):
+        assert runner() is (steps_to_false > 0)
+
+        if steps_to_false <= 0:
+            steps_to_false = inital_steps_to_false
+
+        steps_to_false -= 1
