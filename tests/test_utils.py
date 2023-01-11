8468e7212619803b19d0ad4b78b6f4bafcbe78b2
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-11 21:17:04 +0700

    Test for showly

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 from pytest import mark
 
+from pyhandling.branchers import ActionChain
 from pyhandling.utils import Logger, showly, documenting_by, as_collection, times
 from tests.mocks import Counter, MockHandler, MockObject
 
@@ -15,3 +16,26 @@ def test_number_of_logger_logs(initial_log_number: int, logging_amount: int):
         logger(str())
 
     assert len(logger.logs) == initial_log_number + logging_amount
+
+
+@mark.parametrize(
+    "number_of_handlers, number_of_writer_calls",
+    (
+        ((0, 2), )
+        + tuple(map(lambda x: (x, x + 1), range(1, 4)))
+        + ((31, 32), (63, 64), (127, 128), (515, 516))
+    )
+)
+def test_showly_by_logger(number_of_handlers: int, number_of_writer_calls: int):
+    writing_counter = Counter()
+
+    showly(
+        (
+            MockHandler()
+            if number_of_handlers == 1
+            else ActionChain((MockHandler(), ) * number_of_handlers)
+        ),
+        writer=lambda _: writing_counter()
+    )(None)
+
+    assert writing_counter.counted == number_of_writer_calls
