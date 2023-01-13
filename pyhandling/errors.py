862b6f1676cba15535d4f2bf7a0c7fdebef18e23
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-13 22:46:30 +0700

    Error class containing another error that occurred during the resource handling

--- pyhandling/errors.py
+++ pyhandling/errors.py
@@ -1,3 +1,6 @@
+from pyhandling.tools import DelegatingProperty
+
+
 class PyhandingError(Exception):
     pass
 
@@ -8,3 +11,21 @@ class HandlingRecursionError(PyhandingError):
 
 class HandlingRecursionDepthError(HandlingRecursionError):
     __notes__ = ("To change the limit, call recursive with the max_recursion_depth argument, with the desired value", )
+
+
+class BadResourceError(PyhandingError):
+    """
+    Error class containing another error that occurred during the handling of
+    some resource and the resource itself, respectively.
+    """
+    
+    resource = DelegatingProperty('_resource')
+    error = DelegatingProperty('_error')
+
+    def __init__(self, resource: any, error: Exception):
+        self._resource = resource
+        self._error = error
+
+        super().__init__(
+            f"Resource \"{self.resource}\" could not be handled due to {type(self.error).__name__}: {str(self.error)}"
+        )
