7795fd0e09912df8fab20d7ac44c06807a431326
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-22 22:04:25 +0700

    Importing tools.py when importing from a global module

--- pyhandling/__init__.py
+++ pyhandling/__init__.py
@@ -2,4 +2,5 @@ from pyhandling.annotations import dirty
 from pyhandling.binders import *
 from pyhandling.branchers import *
 from pyhandling.synonyms import *
+from pyhandling.tools import *
 from pyhandling.utils import *
