778ad51ea258bd292578016b08cdf7de3cf4d41c
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-28 23:57:22 +0700

    Infix class for binding functions with arguments

--- pyhandling/language.py
+++ pyhandling/language.py
@@ -8,6 +8,43 @@ from pyhandling.branchers import ActionChain
 from pyhandling.tools import DelegatingProperty, documenting_by
 
 
+class BindingInfix:
+    """
+    Infix class for binding functions with arguments.
+
+    Used in the form \"func |instance| argument\" or \"func |instance* arguments\"
+    if you want to unpack the arguments.
+    """
+
+    binder = DelegatingProperty("_binder")
+    func = DelegatingProperty("_func")
+    arguments = DelegatingProperty("_arguments")
+
+    def __init__(
+        self,
+        binder: Callable[[Callable, ...], Callable],
+        func: Optional[Callable] = None,
+        arguments: Optional[Iterable] = None
+    ):
+        self._binder = binder
+        self._func = func
+        self._arguments = arguments
+
+    def __repr__(self) -> str:
+        return f"BindingInfix for {self.binder.__name__ if hasattr(self.binder, '__name__') else self.binder}"
+
+    def __or__(self, argument: Any) -> Callable:
+        return self._binder(self._func, argument)
+
+    def __ror__(self, func: Callable) -> Special[Self]:
+        return (
+            type(self)(self.binder, func)
+            if self.arguments is None
+            else self._binder(func, *self.arguments)
+        )
+
+    def __mul__(self, arguments: Iterable) -> Callable:
+        return type(self)(self.binder, arguments=arguments)
 
 
 then = documenting_by(
