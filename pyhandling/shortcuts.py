28f96c5128c24c78e0408c7b9dfb798b8c696547
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-01-28 16:49:00 +0700

    Shortcut for post_partial(execute_operation, ...)

--- pyhandling/shortcuts.py
+++ pyhandling/shortcuts.py
@@ -2,10 +2,10 @@ from functools import partial
 from typing import Any, Callable, Iterable
 
 from pyhandling.annotations import binder, event_for, factory_for, handler, decorator
-from pyhandling.binders import close
+from pyhandling.binders import close, post_partial
 from pyhandling.branchers import eventually, ActionChain
-from pyhandling.synonyms import positionally_unpack_to, return_, bind
 from pyhandling.language import then, by, to
+from pyhandling.synonyms import execute_operation, positionally_unpack_to, return_, bind
 from pyhandling.tools import documenting_by, collection_with_reduced_nesting_to, ArgumentPack
 
 
@@ -13,6 +13,13 @@ def callmethod(object_: object, method_name: str, *args, **kwargs) -> Any:
     """Shortcut function to call a method on an input object."""
 
     return getattr(object_, method_name)(*args, **kwargs)
+
+
+operation_by = documenting_by(
+    """Shortcut for post_partial(execute_operation, ...)."""
+)(
+    close(execute_operation, closer=post_partial)
+)
 
 
 event_as: binder = documenting_by(
