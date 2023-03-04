51ad71e3b4d72302c51f80d577a3fc972d8bc4ec
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-03-04 21:30:22 +0700

    BadResourceError refactor in a separate module

---
+++ pyhandling/error_controllers.py
@@ -0,0 +1,24 @@
+from pyhandling.errors import PyhandingError
+from pyhandling.tools import IBadResourceKeeper, DelegatingProperty, open_collection_items
+
+
+class MechanicalError(PyhandingError):
+    pass
+
+
+class BadResourceError(MechanicalError, IBadResourceKeeper, Generic[ResourceT, ErrorT]):
+    """
+    Error class containing another error that occurred during the handling of
+    some resource and the resource itself, respectively.
+    """
+    
+    bad_resource = DelegatingProperty('_bad_resource')
+    error = DelegatingProperty('_error')
+
+    def __init__(self, bad_resource: ResourceT, error: ErrorT):
+        self._bad_resource = bad_resource
+        self._error = error
+
+        super().__init__(
+            f"Resource \"{self._bad_resource}\" could not be handled due to {type(self._error).__name__}: {str(self._error)}"
+        )
--- pyhandling/errors.py
+++ pyhandling/errors.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-from pyhandling.tools import IBadResourceKeeper, DelegatingProperty
 class PyhandingError(Exception):
     pass
 
@@ -17,21 +14,3 @@ class HandlingRecursionDepthError(HandlingRecursionError):
     __notes__ = [
         "To change the limit, call recursive with the max_recursion_depth argument, with the desired value"
     ]
-
-
-class BadResourceError(PyhandingError, IBadResourceKeeper):
-    """
-    Error class containing another error that occurred during the handling of
-    some resource and the resource itself, respectively.
-    """
-    
-    bad_resource = DelegatingProperty('_bad_resource')
-    error = DelegatingProperty('_error')
-
-    def __init__(self, bad_resource: Any, error: Exception):
-        self._bad_resource = bad_resource
-        self._error = error
-
-        super().__init__(
-            f"Resource \"{self._bad_resource}\" could not be handled due to {type(self._error).__name__}: {str(self._error)}"
-        )
