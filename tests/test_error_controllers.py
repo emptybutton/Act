1ff0d6c308050f8fa6ba2b36543fe310237a02d3
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-03-23 18:36:49 +0700

    Refactoring of tests for `errors_from`

--- tests/test_error_controllers.py
+++ tests/test_error_controllers.py
@@ -1,30 +1,36 @@
-from pytest import mark
-
+from pyhandling.error_controllers import errors_from
+from pyhandling.testing import calling_test_case_of
 from tests.mocks import with_attributes
 
 
-@mark.parametrize(
-    "error_storage, result_errors",
-    [
-        (ZeroDivisionError(), (ZeroDivisionError(), )),
-        (with_attributes(error=TypeError()), (TypeError(), )),
+test_errors_from = calling_test_case_of(
+    (lambda: type(errors_from(Exception())[0]), Exception),
+    (lambda: type(errors_from(with_attributes(error=ZeroDivisionError()))[0]), ZeroDivisionError),
+    (
+        lambda: tuple(map(type, errors_from(with_attributes(TypeError, error=ZeroDivisionError()))
+        )),
+        (TypeError, ZeroDivisionError),
+    ),
+    (
+        lambda: tuple(map(type, errors_from(with_attributes(
+            TypeError,
+            error=ZeroDivisionError(),
+            errors=[
+                Exception(),
+                with_attributes(errors=[KeyError(), ValueError()]),
+                with_attributes(error=IndexError()),
+                with_attributes(AttributeError, error=ValueError()),
+                with_attributes(ZeroDivisionError, errors=tuple()),
+                with_attributes(
+                    TypeError,
+                    error=with_attributes(Exception, error=IndexError()),
+                )
+            ],
+        )))),
         (
-            with_attributes(errors=(
-                TypeError(),
-                AttributeError(),
-                with_attributes(error=ZeroDivisionError())
-            )),
-            (TypeError(), AttributeError(), ZeroDivisionError())
-        ), 
-
-    ]
-)
-def test_errors_from(error_storage: error_storage_of[Exception], result_error_types: Iterable[Type[Exception]]):
-    result_errors = errors_from(error_storage)
-    
-    assert len(result_errors) == len(result_error_types)
-    assert all(map(
-        lambda error, error_type: type(error) is error_type,
-        result_errors,
-        result_error_types)
+            TypeError, ZeroDivisionError, Exception, KeyError, ValueError,
+            IndexError, AttributeError, ValueError, ZeroDivisionError, TypeError,
+            Exception, IndexError
+        )
     )
+)
