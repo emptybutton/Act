f1589cff3e984d0ba9a53c70bbd503e3b4242334
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-03-28 20:01:33 +0700

    `__str__` formatting in `Flag`

--- pyhandling/tools.py
+++ pyhandling/tools.py
@@ -144,6 +144,9 @@ class Flag:
     def __repr__(self) -> str:
         return f"<{'positive' if self._sign else 'negative'} {type(self).__name__} \"{self._name}\">"
 
+    def __str__(self) -> str:
+        return self._name
+
     def __hash__(self) -> int:
         return hash(self._name)
 
