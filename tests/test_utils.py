d699b470d161ce439c52b7da3b0ae627797bd0f5
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-03-29 22:57:13 +0700

    Test for `atomically`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -17,6 +17,13 @@ test_context_oriented = calling_test_case_of(
 )
 
 
+test_atomically = calling_test_case_of(
+    (lambda: isinstance(atomically(ActionChain([int, str])), ActionChain), False),
+    (lambda: len(tuple(ActionChain([atomically(ActionChain([int, str])), print]))), 2),
+    (lambda: atomically(lambda a: a)(4), 4)
+)
+
+
 @mark.parametrize(
     "object_, method_name, result",
     (
