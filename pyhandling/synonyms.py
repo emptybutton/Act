30619e3a0d2b2aefa14a4ebf71b98185709c722d
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-04-02 21:51:33 +0700

    Removing custom functions to use syntax

--- pyhandling/synonyms.py
+++ pyhandling/synonyms.py
@@ -12,12 +12,6 @@ __all__ = (
     "collection_of",
     "with_positional_unpacking",
     "with_keyword_unpacking",
-    "with_keyword",
-    "call",
-    "getitem",
-    "setitem",
-    "execute_operation",
-    "transform",
     "to_context",
     "with_context_by",
 )
@@ -62,59 +56,6 @@ def with_keyword_unpacking(func: action_for[ResultT]) -> Callable[[Mapping[str, 
     """
 
     return wraps(func)(lambda arguments: func(**arguments))
-
-
-def with_keyword(argument_name: str, argument_value: Any, func: action_for[ResultT]) -> ResultT:
-    """
-    Atomic partial function for a single keyword argument whose name and value
-    are separate input arguments.
-    """
-
-    return wraps(func)(partial(func, **{argument_name: argument_value}))
-
-
-def call(caller: action_for[ResultT], *args, **kwargs) -> ResultT:
-    """Function to call an input object and return the results of that call."""
-
-    return caller(*args, **kwargs)
-
-
-def getitem(object_: ItemGetter[KeyT, ValueT], item_key: KeyT) -> ValueT:
-    """Function for functional use of `[]` getting."""
-
-    return object_[item_key]
-
-
-def setitem(object_: ItemSetter[KeyT, ValueT], item_key: KeyT, item_value: ValueT) -> None:
-    """Function for functional use of `[]` setting."""
-
-    object_[item_key] = item_value
-
-
-def execute_operation(first_operand: Any, operator: str, second_operand: Any) -> Any:
-    """
-    Function to use operators.
-
-    Since this function uses `eval`, do not pass `operator` from the global
-    input to it.
-    """
-
-    return eval(
-        f"first_operand {operator} second_operand",
-        dict(),
-        {'first_operand': first_operand, 'second_operand': second_operand}
-    )
-
-
-def transform(operand: Any, operator: str) -> Any:
-    """
-    Function to use single operand operator.
-
-    Since this function uses `eval`, do not pass `operator` from the global
-    input to it.
-    """
-
-    return eval(f"{operator} operand", dict(), {'operand': operand})
 
 
 def to_context(action: Callable[[ContextT], ResultT]) -> Callable[[ContextManager[ContextT]], ResultT]:
