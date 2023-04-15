191c39165db90f4085c5682d9e6a39abb51077cd
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-04-15 22:48:22 +0700

    Refactoring `to_context_manager` and `with_context_manager_by` to `with_`

--- pyhandling/synonyms.py
+++ pyhandling/synonyms.py
@@ -3,7 +3,8 @@ from functools import update_wrapper
 from typing import NoReturn, Any, Iterable, Callable, Mapping, Tuple
 from inspect import Signature, Parameter
 
-from pyhandling.annotations import P, ValueT, ResultT, ContextT
+from pyhandling.annotations import P, ValueT, ResultT, ContextT, ErrorHandlingResultT, action_for
+from pyhandling.partials import fragmentarily
 from pyhandling.signature_assignmenting import ActionWrapper, calling_signature_of
 
 
@@ -12,10 +13,9 @@ __all__ = (
     "raise_",
     "assert_",
     "collection_of",
+    "with_",
     "with_unpacking",
     "with_keyword_unpacking",
-    "to_context_manager",
-    "with_context_manager_by",
 )
 
 
@@ -44,6 +44,14 @@ def collection_of(*args: ValueT) -> Tuple[ValueT, ...]:
     """Function to create a `tuple` from unlimited input arguments."""
 
     return args
+
+
+@fragmentarily
+def with_(context_manager: AbstractContextManager, action: action_for[ResultT]) -> ResultT:
+    """Function emulating the `with as` context manager."""
+
+    with context_manager as context:
+        return action(context)
 
 
 class with_unpacking(ActionWrapper):
@@ -78,55 +86,3 @@ class with_keyword_unpacking(ActionWrapper):
 
     def __repr__(self) -> str:
         return f"*{_unpacking_repr(self._action)}"
-
-
-class to_context_manager(ActionWrapper):
-    """Function emulating the `with as` context manager."""
-
-    def __call__(self, context_manager: AbstractContextManager) -> Any:
-        with context_manager as context:
-            return self._action(context)
-
-    @property
-    def _force_signature(self) -> Signature:
-        input_annotation_of_action = tuple(
-            calling_signature_of(self._action).parameters.values()
-        )[0].annotation
-
-        return calling_signature_of(self._action).replace(parameters=[Parameter(
-            "context_manager",
-            Parameter.POSITIONAL_OR_KEYWORD,
-            annotation=AbstractContextManager[input_annotation_of_action]
-        )])
-
-
-class with_context_manager_by:
-    """Function to perform an input action in a specific context."""
-
-    def __init__(
-        self,
-        get_context: Callable[P, AbstractContextManager[ContextT]],
-        action: Callable[P, ResultT],
-    ):
-        self._get_context = get_context
-        self._action = action
-
-        update_wrapper(self, self._action)
-
-    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> ResultT:
-        with self._get_context(*args, **kwargs):
-            return self._action(*args, **kwargs)
-
-    def __repr__(self) -> str:
-        return f"with_context_manager_by({self._get_context}, {self._action})"
-
-
-def _unpacking_repr(action: Callable) -> str:
-    return "*{sep}{action}".format(
-        sep=(
-            '\''
-            if isinstance(self._action, with_unpacking | with_keyword_unpacking)
-            else str()
-        ),
-        action=action,
-    )
