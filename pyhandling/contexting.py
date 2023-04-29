49cddbba2f289900b958991e4fbd94402b8ad451
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-04-29 18:47:05 +0700

    Binary `Context Root` can be created nested

--- pyhandling/contexting.py
+++ pyhandling/contexting.py
@@ -1,14 +1,14 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from functools import update_wrapper
 from inspect import Signature
 from typing import (
-    Generic, Any, Iterator, Callable, Iterable, GenericAlias, TypeVar, Optional
+    Generic, Any, Iterator, Callable, Iterable, GenericAlias, Optional, Self
 )
 
 from pyannotating import Special
 
 from pyhandling.annotations import (
-    ActionT, ErrorT, P, Pm, checker_of, FlagT, A, B, C, V, R
+    ActionT, ErrorT, P, Pm, checker_of, FlagT, A, B, C, V, R, E
 )
 from pyhandling.atoming import atomic
 from pyhandling.flags import nothing, Flag, pointed, FlagVector
@@ -111,26 +111,37 @@ class ContextRoot(ABC, Generic[V, C]):
         )
 
 
-class contextual(ContextRoot, Generic[V, C]):
+class _BinaryForm(ABC, Generic[V, C]):
+    def __init__(self, value: V | Self, *contexts: C):
+        if len(contexts) > 1:
+            value = type(self)(value, *contexts[:-1])
+
+        self._reset(value, nothing if len(contexts) == 0 else contexts[-1])
+
+    @abstractmethod
+    def _reset(self, value: V, context: C) -> None:
+        ...
+
+
+class contextual(ContextRoot, _BinaryForm, Generic[V, C]):
     """Basic `ContextRoot` form representing values with no additional effect."""
 
     value = property_to("_value")
     context = property_to("_context")
 
-    def __init__(self, value: V, when: C = nothing):
+    def _reset(self, value: V, context: C) -> None:
         self._value = value
-        self._context = when
+        self._context = context
 
 
-class contextually(ContextRoot, Generic[ActionT, C]):
+class contextually(ContextRoot, _BinaryForm, Generic[ActionT, C]):
     """`ContextRoot` form for annotating actions with saving their call."""
 
     action = property_to("_value")
     context = property_to("_context")
 
-    def __init__(self, action: Callable[Pm, R], when: C = nothing):
-        self._value = action
-        self._context = when
+    def __init__(self, action: Callable[Pm, R], contexts: C = nothing):
+        _BinaryForm.__init__(self, action, *contexts)
 
         update_wrapper(self, self._value)
         self.__signature__ = self._get_signature()
@@ -141,11 +152,15 @@ class contextually(ContextRoot, Generic[ActionT, C]):
     def __call__(self, *args: Pm.args, **kwargs: Pm.kwargs) -> R:
         return self._value(*args, **kwargs)
 
+    def _reset(self, value: Callable[Pm, R], context: C) -> None:
+        self._action = value
+        self._context = context
+
     def _get_signature(self) -> Signature:
         return call_signature_of(self._value)
 
 
-class ContextualError(Exception, ContextRoot, Generic[ErrorT, C]):
+class ContextualError(Exception, ContextRoot, _BinaryForm, Generic[ErrorT, C]):
     """
     `ContextRoot` form for annotating an error with a context while retaining
     the ability to `raise` the call.
@@ -155,13 +170,15 @@ class ContextualError(Exception, ContextRoot, Generic[ErrorT, C]):
     context = property_to("_context")
 
     def __init__(self, error: ErrorT, context: C):
-        self._value = error
-        self._context = context
-
+        _BinaryForm.__init__(self, error, context)
         super().__init__(repr(self))
 
     def __repr__(self) -> str:
         return f"\"{self._value}\" error when {self._context}"
+
+    def _reset(self, value: ErrorT, context: C) -> None:
+        self._value = value
+        self._context = context
 
 
 class context_pointed(ContextRoot, Generic[ActionT, FlagT]):
