b4734d119499caa3b7f25aaa37b0245123ea732f
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-06-09 18:41:13 +0700

    test(`test_utils.py`): for `Logger`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,4 +24,13 @@ def test_times(steps_to_false: int, number_of_runs: int):
 
 
 @mark.parametrize(
+    'initial_log_number, logging_amount',
+    [(0, 0), (4, 0), (4, 8), (0, 8), (32, 16), (128, 128)]
 )
+def test_number_of_logger_logs(initial_log_number: int, logging_amount: int):
+    logger = Logger((str(), ) * initial_log_number)
+
+    for _ in range(logging_amount):
+        logger(str())
+
+    assert len(logger.logs) == initial_log_number + logging_amount
