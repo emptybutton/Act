915af5dc6593d4fc619a313ea337552b2ccc06ea
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-06-13 22:37:20 +0700

    test(`test_signature_assignmenting.py`): for `annotation_sum`

---
+++ tests/test_signature_assignmenting.py
@@ -0,0 +1,15 @@
+from inspect import Parameter
+
+from pyhandling.signature_assignmenting import *
+from pyhandling.testing import case_of
+
+
+test_annotation_sum = case_of(
+    (lambda: annotation_sum(int, float), int | float),
+    (lambda: annotation_sum(int), int),
+    (lambda: annotation_sum(), Parameter.empty),
+    (
+        lambda: annotation_sum(int, Parameter.empty, float, Parameter.empty, str),
+        int | float | str,
+    ),
+)
