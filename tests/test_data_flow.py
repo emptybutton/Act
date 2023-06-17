8a4326330512d1ac043dd7c88438da307653940f
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-06-17 23:05:51 +0700

    test(`data_flow.py`): removing `test_returnly_call_number`

--- tests/test_data_flow.py
+++ tests/test_data_flow.py
@@ -5,25 +5,12 @@ from pytest import mark, raises
 
 from pyhandling.data_flow import *
 from pyhandling.testing import case_of
-from tests.mocks import Counter
 
 
 test_eventually = case_of(
     (lambda: eventually(lambda a, b: a + b, 100, 28)(1, 2, 3), 128),
     (lambda: eventually(lambda a, b: a + b, 100, 28)(), 128),
 )
-
-
-@mark.parametrize('call_number', tuple(range(4)) + (8, 128, 1024))
-def test_returnly_call_number(call_number: int):
-    call_counter = Counter()
-
-    returnly_proxy_counter = returnly(call_counter)
-
-    for _ in range(call_number):
-        returnly_proxy_counter(2)
-
-    assert call_counter.counted == call_number * 2
 
 
 test_returnly = case_of(
