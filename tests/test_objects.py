13687accfc5edf1717c70555fed23b24d3cb14e6
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-06-17 23:10:31 +0700

    test(`objects.py`): for `like`, `to_attribute`

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -76,3 +76,81 @@ test_of = case_of(
         (MockA, dict(a=1, b=2)),
     ),
 )
+
+
+test_like = case_of(
+    (lambda: like(4)(4), True),
+    (lambda: like(4.)(4), True),
+    (lambda: like(8)(4), False),
+    (lambda: like(None)(None), True),
+    (lambda: like(None)(4), False),
+    (lambda: like('a')('a'), True),
+    (lambda: like('a')('b'), False),
+    (lambda: like([1, 2, 3])([1, 2, 3]), True),
+    (lambda: like([1, 2, 3])([1, 2, 3, 4]), False),
+    (lambda: like((1, 2, 3))((1, 2, 3, 4)), False),
+    (lambda: like([1, 2, 3])((1, 2, 3, 4)), False),
+    (lambda: like([1, 2, 3])((1, 2, 3)), False),
+    (lambda: like(print)(print), True),
+    (lambda: like(print)(pow), False),
+    (lambda: like(str)(str), True),
+    (lambda: like(str)(bool), False),
+    (lambda: like(MockA(1))(MockA(1)), True),
+    (lambda: like(MockA(1))(MockA(2)), False),
+    (lambda: like(MockA(1))(MockB(2)), False),
+    (
+        lambda: like(nested(MockA(1), by=MockA, times=2000))(
+            nested(MockA(2), by=MockA, times=2000),
+        ),
+        False,
+    ),
+    (
+        lambda: like(nested(MockA(1), by=MockA, times=2000))(
+            nested(MockA(1), by=MockA, times=2000),
+        ),
+        True,
+    ),
+)
+
+
+def test_recursive_like():
+    a = MockA(None)
+    b = MockA(None)
+    c = MockA(None)
+    d = MockA(None)
+
+    a.a = c
+    b.a = d
+    c.a = b
+    d.a = a
+
+    assert not like(b)(a)
+
+
+test_to_attribute = case_of((
+    lambda: to_attribute('a', lambda a: a + 5)(MockA(3)).a, 8
+))
+
+
+def test_to_attribute_with_immutability():
+    obj_ = MockA(3)
+
+    result = to_attribute('a', lambda a: a + 5)(obj_)
+
+    assert isinstance(result, MockA)
+    assert result is not obj_
+    assert result.a == 8
+
+
+def test_to_attribute_with_mutability():
+    obj_ = MockA(3)
+
+    result = to_attribute('a', lambda a: a + 5, mutably=True)(obj_)
+
+    assert result is obj_
+    assert result.a == 8
+
+
+test_to_attribute_without_attribute = case_of((
+    lambda: to_attribute('b', lambda b: [b])(MockA(3)).b, [None]
+))
