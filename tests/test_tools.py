cdf944204be350d06082821ba0c8bded9ad9fcaf
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-06-19 22:34:12 +0700

    test(tools.pt): for `LeftCallable`, `to_check`, `as_action`

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,7 +1,18 @@
 from pytest import mark
 
+from pyhandling.testing import case_of
 from pyhandling.tools import *
 from tests.mocks import MockA
+
+
+def test_left_callable():
+    class Action(LeftCallable):
+        def __call__(self, a: int | float) -> int | float:
+            return a ** 2
+
+    result = 4 >= Action()
+
+    assert result == 16
 
 
 @mark.parametrize(
@@ -18,3 +29,16 @@ def test_documenting_by(documentation: str):
 
     assert '__doc__' in mock.__dict__.keys()
     assert mock.__doc__ == documentation
+
+
+test_to_check = case_of(
+    (lambda: to_check(4)(4), True),
+    (lambda: to_check(4)(8), False),
+    (lambda: to_check(bool), bool),
+)
+
+
+test_as_action = case_of(
+    (lambda: as_action(4)(1, 2, 3), 4),
+    (lambda: as_action(pow)(2, 4), 16),
+)
