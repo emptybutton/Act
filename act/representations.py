f36ca73d7f2fa29158c1f59bc35f1c1b9f7bc35a
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-06-26 19:09:09 +0700

    ref(`code_like_repr_of`): for `Any`

--- act/representations.py
+++ act/representations.py
@@ -1,21 +1,21 @@
 from inspect import ismethod, isbuiltin, isfunction
-from typing import Callable
+from typing import Any
 
 
 __all__ = ("code_like_repr_of", )
 
 
-def code_like_repr_of(action: Callable) -> str:
+def code_like_repr_of(value: Any) -> str:
     """
     Function to get a string representation of any value with priority that it
     will match the way it is given in the code.
     """
 
-    if ismethod(action):
-        return f"({code_like_repr_of(action.__self__)}).{action.__name__}"
-    elif isbuiltin(action):
-        return action.__name__
-    elif isfunction(action) or isinstance(action, type):
-        return action.__qualname__
+    if ismethod(value):
+        return f"({code_like_repr_of(value.__self__)}).{value.__name__}"
+    elif isbuiltin(value):
+        return value.__name__
+    elif isfunction(value) or isinstance(value, type):
+        return value.__qualname__
     else:
-        return str(action)
+        return str(value)
