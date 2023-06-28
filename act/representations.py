79f2604b9a56f09df14b88d81b93d2a65ed38f97
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-06-28 22:03:55 +0700

    feat(`code_like_repr_of`): for `str`, `Exception`

--- act/representations.py
+++ act/representations.py
@@ -11,6 +11,8 @@ def code_like_repr_of(value: Any) -> str:
     will match the way it is given in the code.
     """
 
+    from act.contexting import ContextualError
+
     if ismethod(value):
         return f"({code_like_repr_of(value.__self__)}).{value.__name__}"
     elif isbuiltin(value):
@@ -23,5 +25,9 @@ def code_like_repr_of(value: Any) -> str:
             str() if value.stop is None else value.stop,
             str() if value.step in (None, 1) else f":{value.step}",
         )
+    elif isinstance(value, str):
+        return f"'{value}'" if len(value) < 4 else f"\"{value}\""
+    elif isinstance(value, Exception) and not isinstance(value, ContextualError):
+        return f"{type(value).__name__}({code_like_repr_of(str(value))})"
     else:
         return str(value)
