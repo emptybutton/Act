2c8cd36319f0273d4f785fa6a84a4d024ed2c003
TheArtur128 <88573504+TheArtur128@users.noreply.github.com>
2023-10-02 23:05:38 +0700

    feat(`objects`): name enum constructor

--- act/objects.py
+++ act/objects.py
@@ -492,6 +492,10 @@ def struct(type_: type) -> temp:
     })
 
 
+def name_enum_of(annotated: temp(__annotations__=Iterable[str])) -> obj:
+    return obj.of({name: name for name in annotated.__annotations__.keys()})
+
+
 @partially
 def is_templated(attr_name: str, obj_: Special[temp]) -> bool:
     return (
